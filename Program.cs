ebe53812a57aaa7c0c2eb12a589f39ff2924faa4
BilalyC <bilaly.camara@outlook.com>
2018-12-17 17:13:49 +0100

    Exercice 7 Complet

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double ab, bc, ca;
+            Console.WriteLine("Etant donné que le triangle est rectangle en c, veuillez donner les valeur de bc et ca : ");
+            bc = double.Parse(Console.ReadLine());
+            Console.WriteLine("bc = " + bc);
+            ca = double.Parse(Console.ReadLine());
+            Console.WriteLine("ca = " + ca);
+
+            double carre = Math.Pow(bc,2) + Math.Pow(ca,2);
+            ab = Math.Sqrt(carre);
+
+            Console.WriteLine("AB étant l'hypothénuse, sa valeur est la racine carrée de la somme du carré de BC et du carré de CA :");
+            Console.WriteLine("bc^2 + ca^2 = " + carre);
+            Console.WriteLine("ab = " + ab);
+        }
+    }
+}
